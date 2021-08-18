3458587c06e59d27133edfb6233cc8ecfefd9d20
owsei <82065191+OwseiWasTaken@users.noreply.github.com>
2021-08-17 22:08:28 -0300

    removed get func, it's now in util.py
    removed get func and argv = ArgvAssing, this is now done in util.py, ARGV (global) is argv after ArgvAssing

--- python.py
+++ python.py
@@ -4,18 +4,8 @@ from util import *
 
 
 #main
-def Main(argv) -> int:
-	argk = list(argv.keys())
-	def get(*indicators:object) -> list:
-		nonlocal argv,argk
-		other = []
-		for indicator in indicators:
-			if indicator in argk:
-				for item in argv.get(indicator):
-					other.append(item)
-		return other
+def Main() -> int:
 
-	#CODE HERE#
 
 	return 0
 
@@ -23,15 +13,15 @@ def Main(argv) -> int:
 
 
 
-
 #start
 if __name__ == '__main__':
-	argv = ArgvAssing(argv[1:])
 	start = tm()
-	ExitCode = Main(argv)
+	ExitCode = Main()
 
-	if '--debug' in argv.keys():
-		if not ExitCode:printl("%scode successfully exited in " % color["green"])
-		else:printl("%scode exited with error %d in " % (color["red"],ExitCode))
-		print("%.3f seconds%s" % (round(tm()-start,5),color["nc"]))
+	if get('--debug').exists:
+		if not ExitCode:
+			printl("%scode successfully exited in " % color["green"])
+		else:
+			printl("%scode exited with error %d in " % (color["red"],ExitCode))
+		print("%.3f seconds%s" % (tm()-start,color["nc"]))
 	exit(ExitCode)
